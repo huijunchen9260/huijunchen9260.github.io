557e1ebb712252c9d07b56a3857fb6f601767676
huijunchen9260 <chen.9260@osu.edu>
2023-03-27 09:51:17 -0400

    updateweb 03/27/2023 09:51:16

--- pdf/PrincipleMacroSpring2023/genFig.sh
+++ pdf/PrincipleMacroSpring2023/genFig.sh
@@ -3,5 +3,5 @@
 name="$1"
 base="${name%%.*}"
 workdir="${name%/*}/"
-soffice --headless --convert-to pdf --outdir "$workdir" "$1"
+[ -f "$name".pdf ] || soffice --headless --convert-to pdf --outdir "$workdir" "$1"
 pdfseparate "$base.pdf" "$workdir"%d.pdf
