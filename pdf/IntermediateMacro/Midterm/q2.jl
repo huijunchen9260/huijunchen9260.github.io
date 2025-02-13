3a147169ee8e44c34068ade37bcd54a88824cd6d
huijunchen <chen.9260@osu.edu>
2025-02-13 11:02:19 -0500

    updateweb 02/13/2025 11:02:17

--- pdf/IntermediateMacro/Midterm/q2.jl
+++ pdf/IntermediateMacro/Midterm/q2.jl
@@ -21,16 +21,22 @@ tvec = collect( range(0.0001, 0.999, tnum) )
 Gvec = Array{Float64, 1}(undef, tnum)
 
 for indt = 1:1:tnum
+    local t, N, w, G
     t = tvec[indt]
     N = labor(a, b, t)
     w = wage(a, z, N)
     G = gov(w, t, N)
     Gvec[indt] = G
     if (abs(G - Gtarget) < 0.0001)
-        println("At tax = $t, Gval = $G")
+        println("Potential answer for Q42: \
+                At tax = $t, G = $G, \
+                Target - G = $(G - Gtarget)")
     end
 end
 
 Gmax = maximum(Gvec)
 Gmaxidx = argmax(Gvec)
 Tmax = tvec[Gmaxidx]
+
+println("Q43: maximum G is achieved at tax rate $Tmax")
+println("Q44: maximum G is $Gmax")
