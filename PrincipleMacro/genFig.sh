7c6b643c28cf0db261f99688c963c18447ba745c
huijunchen <chen.9260@osu.edu>
2025-06-16 08:19:04 -0400

    updateweb 06/16/2025 08:19:04

--- PrincipleMacro/genFig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-name="$1"
-base="${name%%.*}"
-workdir="${name%/*}/"
-[ -f "$name".pdf ] || soffice --headless --convert-to pdf --outdir "$workdir" "$1"
-pdfseparate "$base.pdf" "$workdir"%d.pdf
